bac49923248deecc12e45f424f1d062d38ad3ad5
Julio Díaz Delgado <107894798+kylekk76@users.noreply.github.com>
2022-08-25 23:15:07 +0100

    Add files via upload

--- M_Pairwise_ Product.py
+++ M_Pairwise_ Product.py
@@ -1,11 +1,26 @@
 
 def maximum_pairwise():
-    nothing = input()
+    nothing = input() # i did this variable because literally they give you a number that if you use it the program is longer
     lst = input().split()
     lst = sorted(list(map(int, lst)),reverse=True)
    
     return int(lst[0])*int(lst[1])
 
+def max_pairwise_product(numbers): # this secondone is what we supouse to do... but i find it way more complicate for nothing
+    n = len(numbers)
+    max_product = 0
+    for first in range(n):
+        for second in range(first + 1, n):
+            max_product = max(max_product,
+                numbers[first] * numbers[second])
+
+    return max_product
+
+
+# if __name__ == '__main__':
+#     _ = int(input())
+#     input_numbers = list(map(int, input().split()))
+#     print(max_pairwise_product(input_numbers))
 
 
 if __name__ =='__main__':
